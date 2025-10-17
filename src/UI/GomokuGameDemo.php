57f0e7ad157e3047661fe3310a93e282f4eaefbc
yangweijie <917647288@qq.com>
2025-10-17 23:49:45 +0800

    优化关闭

--- src/UI/GomokuGameDemo.php
+++ src/UI/GomokuGameDemo.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 namespace phpgo\UI;
 // 五子棋游戏演示程序
 // 基于LibDrawArea抽象类实现的完整五子棋游戏
@@ -8,6 +11,7 @@ use Kingbes\Libui\SDK\LibuiWindow;
 use Kingbes\Libui\SDK\LibuiButton;
 use Kingbes\Libui\SDK\LibuiVBox;
 use Kingbes\Libui\SDK\LibuiHBox;
+use Kingbes\Libui\Window;
 
 /**
  * 五子棋游戏演示
@@ -31,44 +35,58 @@ class GomokuGameDemo
         // 初始化应用
         $this->app = LibuiApplication::getInstance();
         $this->app->init();
-        
+
         // 创建窗口
         $this->window = $this->app->createWindow("五子棋游戏", 550, 650);
-        
+
+        // 设置窗口关闭事件处理，确保只关闭一次
+        Window::onClosing($this->window->getHandle(), function() {
+//            // 确保只执行一次退出
+//            static $isClosing = false;
+//            if ($isClosing) {
+//                return 0; // 阻止关闭
+//            }
+//            $isClosing = true;
+
+            // 退出应用程序
+            $this->app->quit();
+            return 1; // 允许关闭
+        });
+
         // 创建垂直布局容器
         $this->vBox = new LibuiVBox();
         $this->vBox->setPadded(true);
-        
+
         // 创建游戏区域
         $this->gameArea = new GomokuGame(500, 500);
         // 设置游戏区域的父窗口
         $this->gameArea->setParentWindow($this->window->getHandle());
-        
+
         // 创建按钮
         $passButton = new LibuiButton("跳过本轮(Pass Turn)");
         $undoButton = new LibuiButton("撤销上步(Undo Turn)");
         $resignButton = new LibuiButton("认输(Resign)");
         $restartButton = new LibuiButton("重新开始");
-        
+
         // 设置按钮事件处理
         $gameArea = $this->gameArea; // 创建本地引用
-        
+
         $passButton->onClick(function() use ($gameArea) {
             $gameArea->passTurn();
         });
-        
+
         $undoButton->onClick(function() use ($gameArea) {
             $gameArea->undoTurn();
         });
-        
+
         $resignButton->onClick(function() use ($gameArea) {
             $gameArea->resign();
         });
-        
+
         $restartButton->onClick(function() use ($gameArea) {
             $gameArea->resetGame();
         });
-        
+
         // 创建按钮水平布局容器
         $hBox = new LibuiHBox();
         $hBox->setPadded(true);
@@ -76,11 +94,11 @@ class GomokuGameDemo
         $hBox->append($undoButton->getHandle());
         $hBox->append($resignButton->getHandle());
         $hBox->append($restartButton->getHandle());
-        
+
         // 将按钮和游戏区域添加到垂直布局中（按钮在上方）
         $this->vBox->append($hBox->getHandle(), false);  // 按钮区域不拉伸
         $this->vBox->append($this->gameArea->getHandle(), true);  // stretchy = true，使游戏区域填充剩余空间
-        
+
         // 将布局容器设置为窗口的子元素
         $this->window->setChild($this->vBox->getHandle());
         $this->window->show();
